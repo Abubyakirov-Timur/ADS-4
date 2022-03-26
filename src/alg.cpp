25839f7b1b4242c1aad469560c1bddae6cd680cd
Abubyakirov-Timur <99200506+Abubyakirov-Timur@users.noreply.github.com>
2022-03-26 14:15:55 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -11,17 +11,17 @@ int countPairs1(int *arr, int len, int value) {
 int countPairs2(int *arr, int len, int value) {
   int count = 0;
     for (int i = 0; i < len; i++)
-        for (int j = len - 2; j > i; j--)
+        for (int j = len - 1; j > i; j--)
             if (arr[i] + arr[j] == value)
                 count++;
     return count;
 }
 int binar(int *arr, int l, int r, int x) {
-    int lt = l, rt = r, mid = 0; bool f = 0;
+    int lt = l, rt = r, m = 0; bool f = 0;
     while (lt <= rt)  {
         m = (lt + rt) / 2;
         if (arr[m] == x) {
-            return binar(arr, l, m - 1, x) + binar(arr, m + 1, r, x) + 2;
+            return binar(arr, l, m - 1, x) + binar(arr, m + 1, r, x) + 1;
         }
         if (x < arr[m])
             rt = m - 1;
