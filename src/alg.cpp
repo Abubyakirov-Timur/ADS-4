10d8ef186134f8dd4fefdf9df2b3095eca527412
Abubyakirov-Timur <99200506+Abubyakirov-Timur@users.noreply.github.com>
2022-03-26 14:22:26 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,19 +1,19 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  int count = 0;
+    int count = 0;
     for (int i = 0; i < len; i++)
         for (int j = 0; j < len; j++)
-        if (i == j)
+        if (i != j)
             if (arr[i] + arr[j] == value)
                 count++;
     return count / 2;
 }
 int countPairs2(int *arr, int len, int value) {
-  int count = 0;
+    int count = 0;
     for (int i = 0; i < len; i++)
         for (int j = len - 1; j > i; j--)
-            if (arr[i] + arr[j] == value)
-                count++;
+             if (arr[i] + arr[j] == value)
+                 count++;
     return count;
 }
 int binar(int *arr, int l, int r, int x) {
@@ -31,7 +31,7 @@ int binar(int *arr, int l, int r, int x) {
     return 0;
 }
 int countPairs3(int *arr, int len, int value) {
-  int count = 0;
+    int count = 0;
     for (int i = 0; i < len; i++) {
       count += binar(&arr[i + 1], 0 , len - i, value - arr[i]);
     }
