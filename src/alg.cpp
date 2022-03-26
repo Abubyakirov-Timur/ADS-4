7aa128b26fe52de37633ffd30c80973121d29ffb
Abubyakirov-Timur <99200506+Abubyakirov-Timur@users.noreply.github.com>
2022-03-26 12:38:17 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,63 +1,61 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  int k = 0;
+  int w = 0;
   for (int i = 0; i < len - 1; i++) {
     for (int j = i + 1; j < len; j++) {
       if (arr[i] + arr[j] == value)
-        ++k;
+        ++w;
     }
   }
-  return k;
+  return w;
 }
 int countPairs2(int *arr, int len, int value) {
-   int n = 0;
+   int k = 0;
   while ((value - arr[0] >= arr[k]) && (k < len))
-    ++n;
-  int v = 0;
+    ++k;
+  int w = 0;
   int a = 0;
-  int b = n - 1;
+  int b = k - 1;
   while (a != b) {
-    if (arr[a] + arr[b] > value) {
+    if (arr[a] + arr[b] > value)
       --b;
-    } else {
-        if (arr[a] + arr[b] < value) {
-          ++a;
-      } else {
-          for (int i = n - 1; i > a; i--) {
-            if (arr[i] + arr[a] < value)
-              break;
-            if (arr[i] + arr[a] == value)
-              ++v;
-          }
-          ++a;
-        }
+    else if (arr[a] + arr[b] < value)
+      ++a;
+    else {
+      for (int i = k - 1; i > a; i--) {
+        if (arr[i] + arr[a] < value)
+          break;
+        if (arr[i] + arr[a] == value)
+          ++w;
       }
+      ++a;
+    }
   }
-  return v;
+  return w;
 }
 int countPairs3(int *arr, int len, int value) {
-  int m = 0;
-  int h = 0;
-  while ((value - arr[0] >= arr[h]) && (h < len))
-    ++h;
-  for (int i = 0; i < h - 1; i++) {
-    int y = value - arr[i];
-    int a = i + 1;
-    int b = h - 1;
+  int w = 0;
+  int k = 0;
+  while ((value - arr[0] >= arr[k]) && (k < len))
+    ++k;
+  for (int i = 0; i < k / 2; i++) {
+    int x = value - arr[i];
+    int a = k / 2;
+    int b = k - 1;
     bool f = false;
     while ((a <= b) && (!f)) {
       int cen = (a + b) / 2;
-      if (arr[cen] == y) {
+      if (arr[cen] == x) {
         for (int j = a; j <= b; j++)
-          if (arr[j] == y)
-            ++m;
+          if (arr[j] == x)
+            ++w;
         f = true;
       }
-      if (arr[cen] > y)
+      if (arr[cen] > x)
         b = cen - 1;
       else
         a = cen + 1;
     }
   }
-  return m;
+  return w;
 }
